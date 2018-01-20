7f456bf1f4c01da1f81e94967370461406a51a87
colorbash <colorbash3000@gmail.com>
2018-01-20 14:15:56 +0300

    now it's my page

---
+++ client.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function (){
+    $("#scroll2bio-id").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#bio-id").offset().top
+        }, 700);
+    });
+});
+$(document).ready(function (){
+    $("#scroll2bio-id2").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#bio-id").offset().top
+        }, 700);
+    });
+});
+$(document).ready(function (){
+    $("#scroll2contacts-id").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#contacts-id").offset().top
+        }, 700);
+    });
+});
